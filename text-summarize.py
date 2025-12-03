b527ca7893c01e015b495c6c94022652dcd1866e
Ganesh <ganesh.ayer1987@gmail.com>
2025-12-02 20:46:18 -0600

    Update text-summarize.py

--- text-summarize.py
+++ text-summarize.py
@@ -11,7 +11,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 groq_api_key= os.getenv('GROQ_API_KEY')
-st.secrets["GROQ_API_KEY"]
 model_groq=ChatGroq(model="llama-3.1-8b-instant",groq_api_key=groq_api_key)
 
 # System prompt
