7458126c302ed746e4be4882e813a7eca44f0e56
Ganesh <ganesh.ayer1987@gmail.com>
2025-12-03 00:01:35 -0600

    Update text-summarize.py

--- text-summarize.py
+++ text-summarize.py
@@ -13,7 +13,8 @@ load_dotenv()
 
 groq_api_key=  os.getenv('GROQ_API_KEY')
 model_groq=ChatGroq(model="llama-3.1-8b-instant",groq_api_key=groq_api_key)
-
+if not os.getenv("USER_AGENT"):
+    os.environ["USER_AGENT"] = "my-streamlit-app"
 
 # System prompt
 system_prompt = """
